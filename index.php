b95842fe4622ff299a3c1b6a9885a8950389944f
deivgt <deivtg@gmail.com>
2015-05-21 15:40:22 -0600

    testllslslslslslslslsls

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ else {}?>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">DACOTRANS</a>
+          <a class="navbar-brand" href="#">DACOTRAN S dads</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
